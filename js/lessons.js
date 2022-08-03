1d919ee6f4326eadceab9d85311f25a990f189d9
THI TRIEU XUYEN NGUYEN <u1252970@umail.utah.edu>
2022-08-02 19:29:56 -0600

    Fixed missing lesson

--- js/lessons.js
+++ js/lessons.js
@@ -12,49 +12,35 @@ const lesson09 = [...GenerateLessonBasic(RomanToBopomofo("ty"))];
 const lesson10 = [...GenerateLessonBasic(RomanToBopomofo("ui"))];
 const lesson11 = [...GenerateLessonBasic(RomanToBopomofo("op"))];
 const lesson12 = () => [...GenerateLessonRandom(RomanToBopomofo("qwertyuiop"))];
-const lesson = [...GenerateLessonBasic(RomanToBopomofo("zx"))];
-const lesson13 = [...GenerateLessonBasic(RomanToBopomofo("cv"))];
-const lesson14 = [...GenerateLessonBasic(RomanToBopomofo("bn"))];
-const lesson15 = [...GenerateLessonBasic(RomanToBopomofo("m,"))];
-const lesson16 = [...GenerateLessonBasic(RomanToBopomofo("./"))];
-const lesson17 = () => [...GenerateLessonRandom(RomanToBopomofo("zxcvbnm,./"))];
-const lesson18 = [...GenerateLessonBasic(RomanToBopomofo("12"))];
-const lesson19 = [...GenerateLessonBasic(RomanToBopomofo("34"))];
-const lesson20 = [...GenerateLessonBasic(RomanToBopomofo("67"))];
-const lesson21 = [...GenerateLessonBasic(RomanToBopomofo("58"))];
-const lesson22 = [...GenerateLessonBasic(RomanToBopomofo("90"))];
-const lesson23 = [...GenerateLessonBasic(RomanToBopomofo("-1"))];
-const lesson24 = () => [
-  ...GenerateLessonRandom(RomanToBopomofo("1234567890-")),
-];
-const lesson25 = () => [
-  ...GenerateLessonRandom(
-    RomanToBopomofo("1234567890-qwertyuiopasdfghjkl;zxcvbnm,./")
-  ),
-];
-const lesson26 = () => [
-  ...GenerateLessonRandom(
-    RomanToBopomofo("1234567890-qwertyuiopasdfghjkl;zxcvbnm,./")
-  ),
-];
-const lesson27 = () => [
-  ...GenerateLessonRandom(
-    RomanToBopomofo("1234567890-qwertyuiopasdfghjkl;zxcvbnm,./")
-  ),
-];
-const lesson28 = [
+const lesson13 = [...GenerateLessonBasic(RomanToBopomofo("zx"))];
+const lesson14 = [...GenerateLessonBasic(RomanToBopomofo("cv"))];
+const lesson15 = [...GenerateLessonBasic(RomanToBopomofo("bn"))];
+const lesson16 = [...GenerateLessonBasic(RomanToBopomofo("m,"))];
+const lesson17 = [...GenerateLessonBasic(RomanToBopomofo("./"))];
+const lesson18 = () => [...GenerateLessonRandom(RomanToBopomofo("zxcvbnm,./"))];
+const lesson19 = [...GenerateLessonBasic(RomanToBopomofo("12"))];
+const lesson20 = [...GenerateLessonBasic(RomanToBopomofo("34"))];
+const lesson21 = [...GenerateLessonBasic(RomanToBopomofo("67"))];
+const lesson22 = [...GenerateLessonBasic(RomanToBopomofo("58"))];
+const lesson23 = [...GenerateLessonBasic(RomanToBopomofo("90"))];
+const lesson24 = [...GenerateLessonBasic(RomanToBopomofo("-1"))];
+const lesson25 = () => [...GenerateLessonRandom(RomanToBopomofo("1234567890-"))];
+const lesson26 = () => [...GenerateLessonRandom(RomanToBopomofo("1234567890-qwertyuiopasdfghjkl;zxcvbnm,./"))];
+const lesson27 = () => [...GenerateLessonRandom(RomanToBopomofo("1234567890-qwertyuiopasdfghjkl;zxcvbnm,./"))];
+const lesson28 = () => [...GenerateLessonRandom(RomanToBopomofo("1234567890-qwertyuiopasdfghjkl;zxcvbnm,./"))];
+const lesson29 = [
   ..."ㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢ ㄅㄉˇˋㄓˊ˙ ㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢㄅㄆㄇㄈ ㄉㄊㄋㄌ ˇㄍㄎㄏ ˋㄐㄑㄒㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢ ㄅㄉˇˋㄓˊ˙ ㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢㄅㄆㄇㄈ ㄉㄊㄋㄌ ˇㄍㄎㄏ ˋㄐㄑㄒㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢ ㄅㄉˇˋㄓˊ˙ ㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢㄅㄆㄇㄈ ㄉㄊㄋㄌ ˇㄍㄎㄏ ˋㄐㄑㄒㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢ ㄅㄉˇˋㄓˊ˙ ㄅㄉˇˋㄓ ˊ˙ㄚㄞㄢㄅㄆㄇㄈ ㄉㄊㄋㄌ ˇㄍㄎㄏ ˋㄐㄑㄒ",
 ];
-const lesson29 = [
+const lesson30 = [
   ..."ㄋㄧˇ ㄨㄛˇ ㄋㄧˇ ㄨㄛˇ ㄋㄧˇ ㄨㄛˇ  ㄋㄧˇ ㄨㄛˇ ㄋㄧˇ ㄨㄛˇ ㄋㄧˇ ㄨㄛˇ  ㄓㄜˋ ㄋㄚˇ ㄓㄜˋ ㄋㄚˇ ㄓㄜˋ ㄋㄚˇ  ㄓㄜˋ ㄋㄚˇ ㄓㄜˋ ㄋㄚˇ ㄓㄜˋ ㄋㄚˇ  ㄏㄠˇ ㄏㄜ ㄏㄠˇ ㄏㄜ ㄏㄠˇ ㄏㄜ ㄔㄚˊ ㄐㄧㄠˋ ㄐㄧㄠˋ ㄐㄧㄠˋ ㄐㄧㄠˋ ㄐㄧㄠˋ ㄏㄜ ㄔㄚˊ ㄏㄜ ㄔㄚˊ ㄏㄜ ㄔㄚˊ ㄑㄧㄥˇㄑㄧㄥˇ ㄑㄧㄥˇ ㄑㄧㄥˇ ㄑㄧㄥˇ ㄑㄧㄥˇ ㄕㄣˊ ˙ㄇㄜ ㄕㄣˊ ˙ㄇㄜ ㄕㄣˊ ˙ㄇㄜ  ㄒㄧㄥˋ ㄒㄧㄥˋ ㄒㄧㄥˋ ㄒㄧㄥˋ ㄒㄧㄥˋ ",
 ];
-const lesson30 = [
+const lesson31 = [
   ..."ㄊㄞˊ ㄨㄢ ㄊㄞˊ ㄨㄢ ㄊㄞˊ ㄨㄢ ˙ㄇㄚ ˙ㄇㄚ ˙ㄇㄚ ˙ㄇㄚ ˙ㄇㄚ ˙ㄇㄚ ˙ㄇㄚ  ㄨ ㄌㄨㄥˊ ㄔㄚˊ ㄨ ㄌㄨㄥˊ ㄔㄚˊ ㄧㄠˋㄖˋ ㄅㄣˇ ㄇㄟˇ ㄍㄨㄛˊ ㄩㄝˋ ㄋㄢˊ  ㄖˋ ㄅㄣˇ ㄇㄟˇ ㄍㄨㄛˊ ㄩㄝˋ ㄋㄢˊ  ㄉㄨㄛ ㄕㄠˇ ㄉㄨㄛ ㄕㄠˇ ㄉㄨㄛ ㄕㄠˇ  ㄅㄞˇ ㄎㄨㄞˋ ㄅㄞˇ ㄎㄨㄞˋ ㄓ ㄓ ㄓ  ㄆㄧㄠˋ ㄌㄧㄤˋ ㄆㄧㄠˋ ㄌㄧㄤˋ ㄉㄨㄟˋ ㄉㄨㄟˋ ㄉㄨㄟˋ ㄉㄨㄟˋ ㄉㄨㄟˋ ㄉㄨㄟˋ ㄊㄧㄥ ㄊㄧㄥ ㄊㄧㄥ ㄊㄧㄥ ㄊㄧㄥ ㄊㄧㄥ  ",
 ];
-const lesson31 = [
+const lesson32 = [
   ..."ㄍㄨㄟˋ ㄍㄨㄟˋ ㄍㄨㄟˋ ㄍㄨㄟˋ ㄍㄨㄟˋ ㄐㄧㄚ ㄐㄧㄚ ㄐㄧㄚ ㄐㄧㄚ ㄐㄧㄚ ㄐㄧㄚ  ㄗˋ ㄐㄧˇ ㄗˋ ㄐㄧˇ ㄗˋ ㄐㄧˇ ㄗˋ  ㄕˋ ˙ㄚ ㄕˋ ˙ㄚ ㄕˋ ˙ㄚ ㄕˋ ˙ㄚ  ㄎㄜˇ ㄧˇ ㄎㄜˇ ㄧˇ ㄎㄜˇ ㄧˇ ˙ㄉㄜ ㄘㄞˋ ㄘㄞˋ ㄘㄞˋ ㄘㄞˋ ㄘㄞˋ ㄘㄞˋ  ㄕㄤˋ ㄨㄤˇ ㄕㄤˋ ㄨㄤˇ ㄕㄤˋ ㄨㄤˇ  ㄈㄥ ㄐㄧㄥˇ ㄈㄥ ㄐㄧㄥˇ ㄈㄥ ㄐㄧㄥˇ  ㄓㄜˋ ㄌㄧˇ ㄓㄜˋ ㄌㄧˇ ㄓㄜˋ ㄌㄧˇ  ㄒㄩㄝˊ ㄕㄥ ㄒㄩㄝˊ ㄕㄥ ㄒㄩㄝˊ ㄕㄥ  ",
 ];
-const lesson32 = [
+const lesson33 = [
   ..."ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ  ㄙㄨˋ ㄕㄜˋ ㄙㄨˋ ㄕㄜˋ ㄙㄨˋ ㄕㄜˋ  ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ  ㄒㄩㄝˊ ㄒㄩㄝˊ ㄒㄩㄝˊ ㄒㄩㄝˊ ㄒㄩㄝˊ ㄕㄡˇ ㄐㄧ ㄕㄡˇ ㄐㄧ ㄕㄡˇ ㄐㄧ ㄐㄧㄡˋㄖㄣˊ ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ ㄖㄣˊ  ㄙㄨˋ ㄕㄜˋ ㄙㄨˋ ㄕㄜˋ ㄙㄨˋ ㄕㄜˋ  ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ ㄑㄩˋ  ㄒㄩㄝˊ ㄒㄩㄝˊ ㄒㄩㄝˊ ㄒㄩㄝˊ ㄒㄩㄝˊ ㄕㄡˇ ㄐㄧ ㄕㄡˇ ㄐㄧ ㄕㄡˇ ㄐㄧ ㄐㄧㄡˋ",
 ];
 
@@ -91,6 +77,7 @@ const lessons = [
   lesson30,
   lesson31,
   lesson32,
+  lesson33,
 ];
 
 function GenerateLessonRandom(charArr) {
